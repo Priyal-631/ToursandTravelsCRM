592f18f89137ffed5d863e67094a667ff416d2bf
Rhea <rheamisquitta@student.sfit.ac.in>
2026-04-04 20:10:00 +0530

    fix prisma import

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,6 +1,10 @@
-import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
-import pg from 'pg'
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
+
+import pgPkg from '@prisma/adapter-pg';
+const { PrismaPg } = pgPkg;
+
+import pg from 'pg';
 
 const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL
